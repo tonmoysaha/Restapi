21d6dd66782673835b0df08a791fd3188d783468
tonmoysaha <tonmoysahaopi@gmail.com>
2019-08-22 13:24:20 +0600

    usingpathvariable

--- src/main/java/com/spring/restcontroller/StudentRestController.java
+++ src/main/java/com/spring/restcontroller/StudentRestController.java
@@ -3,7 +3,10 @@ package com.spring.restcontroller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +16,26 @@ import com.spring.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	@GetMapping("/students")
-	public List<Student> getStudents(){
-		List<Student> students = new ArrayList<Student>();
+	private List<Student> students;
+	
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<Student>();
 		students.add(new Student("opi", "saha"));
 		students.add(new Student("piki","saha"));
+		students.add(new Student("ma","love"));
+	}
+	
+	
+	@GetMapping("/students")
+	public List<Student> getStudents(){
 		
 		return students;
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		return students.get(studentId);
 	}
 
 }
